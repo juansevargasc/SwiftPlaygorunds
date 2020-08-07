650227b8f7468c2d944fa9fe83f0558c0d35e179
Juanse <jsvargasc@unal.edu.co>
2020-08-06 21:55:15 -0500

    Starting new lessons. Operations

---
+++ 06_operations.playground/Contents.swift
@@ -0,0 +1,61 @@
+import UIKit
+
+//Operadores
+
+/*
+ 1. Unarios
+ 2. Binarios
+ 3. Ternarios
+ */
+
+let b = 10
+var a = 5
+a = b
+
+let (x,y) = (1,2)
+
+if a == b
+{
+    print("a y b son iguales")
+}
+
+1 + 2
+5 - 3
+2 * 3
+10.0 / 2.5
+"Hello " + "World!"
+
+//Enteros
+9 / 4
+9 % 4
+9 == 4*2 + 1
+
+-9 / 4
+-9 % 4
+
+let five = 5
+let minusFive = -five
+let plusFive = -minusFive
+let minusSix = -6
+let alsoMinusSix = +minusSix
+
+
+
+//Ejericio
+9 / -4.3
+9 % 4
+9 == 4*2 + 1
+
+//Ejericio
+var decimalNumber = 20.0
+let reminderDouble = decimalNumber.truncatingRemainder(dividingBy: 3.0)
+print(reminderDouble)
+
+var number = 9
+number += 3 //number = number + 3
+number -= 2
+
+number /= 2
+number *= 12
+number %= 7 //number = number % 7
+
