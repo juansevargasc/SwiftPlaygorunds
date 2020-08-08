1b827197b215d2da5eea13ae2afa2f16f9907678
Juanse <jsvargasc@unal.edu.co>
2020-08-08 17:51:38 -0500

    Multiline strings, and exercise

---
+++ 07_strings.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+let someString = "Soy un string cualquiera"
+
+let multilineString = """
+
+
+    Soy Juanse\
+    Estamos haciendo el curso Swift.
+    Intro
+"""
+//El backslash anula los intros
+
+print(multilineString)
+
+let wiseWords = "\"La imaginación es más importante que el saber\" -Albert Einstein"
+let dollarSign = "\u{24}"
+let blackHeart = "\u{2665}"
+let heart = "\u{1F496}"
+
+//Ejercicio
+let nameAndCountry = "Hola soy Juanse, de \u{1F1E8}\u{1F1F4}"
+
