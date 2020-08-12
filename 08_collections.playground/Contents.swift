d03d447f50e9bd58ae6c15e9f3ee537c805852a7
Juanse <jsvargasc@unal.edu.co>
2020-08-11 20:57:05 -0500

    Arrays

---
+++ 08_collections.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+//ESTRUCTURAS DE DATOS
+
+//Arrays
+//Conjuntos
+//Diccionarios
+
+//Array
+var someInts = [Int]()
+someInts.count
+someInts.append(31)
+someInts.count
+
+someInts = []
+someInts.count
+
+var someDoubles = Array(repeating: 3.141592, count: 3)
+someDoubles.count
+
+var moreDoubles = Array(repeating: 2.5, count: 4)
+
+var aLotOfDoubles = someDoubles + moreDoubles
+aLotOfDoubles.count
+
+var shoppingList: [String] = ["Papas", "Pimiento", "Tortillas", "Cerdo", "Cebolla"]
+shoppingList.count
+
+
+//Ejercicio
+var family: [String] = ["Jose", "Maria Helena", "Gina", "Nelson", "Jorge", "Diana", "Jimeno", "Sarita", "Sandra"]
+family.count
