8cc645492e48c6e37874af45adf7f6ae759ce317
Juanse <jsvargasc@unal.edu.co>
2020-08-13 13:17:11 -0500

    Switch compound, tuples

--- 09_control_flow.playground/Contents.swift
+++ 09_control_flow.playground/Contents.swift
@@ -126,9 +126,104 @@ if temp <= 15
 //Ejercicio prestar dinero
 
 
+//Switch - Case
+let someCharacter: Character = "z"
+switch someCharacter
+{
+    case "a", "A":
+        print("Es la primera letra del alfabeto")
+    case "z", "Z"   :
+        print("Es la última letra del alfabeto")
+    default:
+        print("Alguna otra letra")
+}
 
 
+//Ejercicio de Kendall
+let someCharacterVocal: Character = "a"
+switch someCharacterVocal.uppercased() { //.uppercased transforma el valor dado en Mayuscula, por lo que no importa si la letra digitada es Mayuscula o Minuscula
+    case"A","E","I","O","U":
+        print("La letra \(someCharacterVocal.uppercased()) es una vocal")
+    default:
+        print("Podria ser una consonante")
+}
 
 
+//Switch usando rangos - Interval Matching
+let moons = 62
+let phrase = "Lunas en saturno"
+let naturalCount: String
+
+switch moons {
+    case 0:
+        naturalCount = "No hay "
+    case 1..<5:
+        naturalCount = "Hay unas pocas "
+    case 5..<12:
+        naturalCount = "Hay bastantes "
+    case 12..<100:
+        naturalCount = "Hay decenas de "
+    case 100..<1000:
+        naturalCount = "Hay centenares "
+    default:
+        naturalCount = "Hay muchiiiisimas "
+}
+print("\(naturalCount)\(phrase)")
+
+//Ejercicio Dinero
 
 
+//Switch Usando Tuplas
+let somePoint = (0, 1)
+switch somePoint {
+case (0, 0):
+    print("El punto \(somePoint) es el origen de las coordenadas")
+case(_, 0):
+    print("El punto \(somePoint) se halla sobre el eje de las X")
+case(0, _):
+    print("El punto \(somePoint) se halla sobre el eje de las Y")
+case(-2...2, -2...2):
+    print("El punto \(somePoint) se halla sobre el cuadrado de lado 4")
+default:
+    print("El punto está en algún lado")
+}
+
+let anotherPoint = (2, 0)
+switch anotherPoint
+{
+    case (let x, 0): //Guarda con let variable, el valor para poder usarlo
+        print("Sobre el eje de las X, con valor \(x)")
+    case (0, let y):
+        print("Sobre el eje de las Y, con valor \(y)")
+    case let(x, y) where x == y:
+        print("El punto se halla sobre la recta x = y")
+    case let(x, y) where x == -y:
+        print("El punto se halla sobre la recta x = -y")
+    case let (x, y): //En este caso x e y pueden tomar cualquier valor, por tanto el default no es ncesario.
+        print("En algún lugar del plano, en ( \(x), \(y) )")
+}
+
+
+//Switch con casos compuestos
+let someCharacters: Character = " "
+switch someCharacters
+{
+case "a", "e", "i", "o", "u":
+    print("Se trata de una vocal")
+case "b", "c", "d", "f", "g": //TODO Acabar con el resto del abecedeario, solo consonantes
+    print("Se trata de una consonante")
+default:
+    print("Se trata de un caracter no vocal ni consonante minúscula")
+}
+
+
+let stillAnotherPoint = (9, 5)
+switch stillAnotherPoint
+{
+    case (let distance, 0), (0, let distance):
+        print("Se halla sobre el eje a distancia \(distance)")
+    default:
+        print("No está sobre el eje")
+}
+
+//TODO: Switch ejemplo
