5f566a64943b7074a1c96cd560b41a4cae0ce926
Juanse <jsvargasc@unal.edu.co>
2020-08-12 19:31:37 -0500

    Sets, Iterations and operations of sets, dictionaries, iteration of dictionaries

--- 08_collections.playground/Contents.swift
+++ 08_collections.playground/Contents.swift
@@ -82,3 +82,164 @@ for (ind, member) in family.enumerated()
 {
     print("Family member: \(ind + 1): \(member)")
 }
+
+
+
+//Conjuntos (Set)
+var letters = Set<Character>()
+letters.count
+letters.insert("a")
+letters.insert("h")
+letters.insert("b")
+letters
+
+var favoriteGames: Set<String> = ["Final Fantasy", "World of Warcraft", "Farcry"]
+favoriteGames.count
+
+if favoriteGames.isEmpty
+{
+    print("No hay juegos favoritos")
+}else{
+    print("")
+}
+
+favoriteGames.insert("Metal Gear")
+
+if let removedGame = favoriteGames.remove("Farcry")
+{
+    print("He eliminado de la lista \(removedGame)")
+}
+
+if favoriteGames.contains("Metal Gear")
+{
+    print("Me encanta ese juego")
+}
+
+for vg in favoriteGames.sorted()
+{
+    print(vg)
+}
+
+
+
+//Ejercicio
+var favoriteMovies: Set<String> = ["Man Of Steel", "Batman v. Superman", "Zack Snyder Justice League", "Joker", "Aquaman"]
+favoriteMovies.count
+
+favoriteMovies.insert("Man Of Steel")
+for movie in favoriteMovies.sorted()
+{
+    print(movie)
+}
+
+
+
+//Iterations and operations of sets
+let oddDigits: Set = [1, 3, 5, 7, 9]
+let evenDigits: Set = [0, 2, 4, 6, 8]
+let primeNumbers: Set = [2, 3, 5, 7]
+
+//A union B
+oddDigits.union(evenDigits).sorted()
+//A intersección B
+oddDigits.intersection(evenDigits)
+evenDigits.intersection(evenDigits)
+oddDigits.intersection(primeNumbers).sorted()
+
+//A-B Diferencia simétrica
+oddDigits.subtracting(primeNumbers).sorted()
+
+//A+B = (A-B) unión (B-A) = A o B
+oddDigits.symmetricDifference(primeNumbers).sorted()
+
+let houseAnimals: Set = ["🐶", "😺"]
+let farmAnimals: Set = ["🐮", "🐔", "🐑", "🐶", "😺"]
+let cityAnimals: Set = ["🐀", "🕊"]
+
+houseAnimals.isSubset(of: farmAnimals)
+farmAnimals.isSuperset(of: houseAnimals)
+farmAnimals.isDisjoint(with: cityAnimals) //Son disjuntos si su intersección es vacía
+
+//Ejercicio
+let myFavoriteFood: Set = ["Guacamole", "Arroz", "Café", "Zanahoria", "Zukini", "Tomate", "Berenjena", "Lenteja", "Garbanzo", "Pollo", "Hamburguesa", "Panini", "Manzana", "Fresa", "Queso"]
+let frutas: Set = ["Curuva", "Guanabana", "Fresa", "Mora", "Mandarina", "Naranja", "Manzana", "Banano"]
+
+myFavoriteFood.isSubset(of: frutas) //Subconjunto
+myFavoriteFood.isSuperset(of: frutas) //Superconjunto
+myFavoriteFood.intersection(frutas) //Intersección
+myFavoriteFood.union(frutas) //Unión
+myFavoriteFood.subtracting(frutas) //Diferencia
+myFavoriteFood.symmetricDifference(frutas) //Diferencia simétrica
+
+for item in myFavoriteFood.symmetricDifference(frutas)
+{
+    print(item, separator: " ", terminator: " ")
+}
+
+
+//Diccionarios Parejas: Clave - Valor --> [k1:v1, k2:v2, ...]
+var nameOfIntegers = [Int : String]()
+nameOfIntegers[15] = "Quince"
+nameOfIntegers = [:] //Vaciarlo
+
+var airports: [String : String] = ["YYZ" : "Toronto",
+                                   "DUB" : "Dublin",
+                                   "PMI" : "Palma de Mallorca"
+]
+//Par clave - valor debe ser único
+airports.count
+airports.isEmpty
+
+airports["LHR"] = "London City Airport"
+airports
+airports["LHR"] = "London Heathrow"
+airports
+
+if let oldValue = airports.updateValue("Dublin Airport", forKey: "DUB") //Sobreescribe el valor
+{
+    print("El valor tenía antiguamente el nombre de \(oldValue) ")
+}
+airports
+
+if let airportName = airports["DUB"]
+{
+    print("El aeropuerto de DUB es \(airportName)")
+}
+
+airports["PMI"] = nil
+airports
+
+if let removedAirport = airports.removeValue(forKey: "DUB")
+{
+    print("Aeropuerto removido: \(removedAirport)")
+}
+airports
+
+//Ejercicio
+var myFamily: [String : Int] = ["Sandra" : 52, "Sarita" : 18]
+myFamily["Juanse"] = 26
+myFamily["Jimeno"] = 15
+myFamily
+
+
+//Iteración en Diccionarios
+//Keys y Values
+for (key, value) in airports
+{
+    print("\(key) - \(value)")
+}
+
+//Solo Keys
+for airportKey in airports.keys
+{
+    print(airportKey)
+}
+//Solo Value
+for airportName in airports.values
+{
+    print(airportName)
+}
+//Arrays a partir de keys o values de diccionario
+let airportKeys = [String](airports.keys.sorted())
+let airportNames = [String](airports.values.sorted())
+
