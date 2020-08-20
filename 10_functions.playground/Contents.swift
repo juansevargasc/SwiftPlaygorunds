466cc52c400ebe99c7782532acb8a8498901d715
Juanse <jsvargasc@unal.edu.co>
2020-08-20 16:58:04 -0500

    Function types, parámetros inout, param y variadics

--- 10_functions.playground/Contents.swift
+++ 10_functions.playground/Contents.swift
@@ -103,17 +103,128 @@ minMax(array: []) //Devuelve Nil
 
 
 //Etiquetas Param y Variadics
-func someFunction(f1 firstParamName: Int, f2 secondParamName: Int)
+func someFunction(f1 firstParamName: Int, f2 secondParamName: Int = 12) //Segundo parámetro por defecto es 12
 {
     print(firstParamName + secondParamName)
 }
 
 someFunction(f1: 5, f2: 1)
+someFunction(f1: 5)
 
-
-func greeting(person: String, hometown: String) -> String
+func greeting(_ person: String, from hometown: String) -> String //_ omite etiqueta en person
 {
     return "Hola \(person) un placer que nos visites desde \(hometown)"
 }
 
+greeting("Juan Gabriel", from: "Mallorca")
+
+func mean(_ numbers: Double...) -> Double //Múltiples parámetros, mínimo uno sintaxis: tag name: type...
+{
+    var total: Double = 0
+    for number in numbers {
+        total += number
+    }
+    return total / Double(numbers.count)
+}
+
+mean(1, 2, 3, 5)
+mean(2, 2, 2, 2)
+mean(3, 4, 10, 3, 1)
+
+func geometric(_ numbers: Double...) -> Double
+{
+    var total: Double = 1
+    for number in numbers
+    {
+        total *= number
+    }
+    return pow(total, 1 / ( Double(numbers.count)) )
+}
+
+
+geometric(4, 3)
+geometric(2, 4)
+geometric(4, 4)
+geometric(1, 25)
+
+//Parámetros inout
+var x = 5
+
+func addOne(number: Int)
+{
+    var number2 = number
+    number2 += 1
+    print("El número ahora vale \(number2)")
+}
+
+
+addOne(number: x)
+
+func swapTwoInts(_ a: inout Int, _ b: inout Int) //Inout significa que se hace referencia al espacio de memoria. Como al apuntador.
+{
+    let tempA = a
+    a = b
+    b = tempA
+}
+
+var someInt = 3
+var otherInt = 7
+print("Someint: \(someInt). Otherint: \(otherInt)")
+swapTwoInts(&someInt, &otherInt)
+print("Someint: \(someInt). Otherint: \(otherInt)")
+
+
+//Function Types
+func addTwoInts(_ a: Int, _ b: Int) -> Int
+{
+    return a + b
+} // (Int, Int) -> Int    tipo de dato Function
+
+func multiplyTwoInts(_ a: Int, _ b: Int) -> Int
+{
+    return a * b
+}// (Int, Int) -> Int
+
+func printHelloWorld()
+{
+    print("Hello World")
+} //() -> Void   Vacío de entrada y vacío de salida
+
+//Cualquier función puede ser creada como variable.
+var matchFunction: (Int, Int) -> Int = addTwoInts //Puedo cambiar de función
+matchFunction(4, 6) //Variable función o función guardada en otra variable.
+
+func printMathResult(_ mathFunction: (Int, Int) -> Int, _ a: Int, _ b: Int)
+{
+    print("Resultado: \(mathFunction(a, b) )")
+}
+
+printMathResult(multiplyTwoInts, 5, 9)
+
+func stepForward(_ input: Int) -> Int
+{
+    return input + 1
+}
+
+func stepBackward(_ input: Int) -> Int
+{
+    return input - 1
+}
+
+func chooseStepFunction(backward: Bool) -> (Int) -> Int //Devuelve una función que acepta un (Int) como parámetro de entrada y un Int como parámetro de salida
+{
+    return backward ? stepBackward : stepForward
+}
+
+var value = -6
+
+let moveNearerZero = chooseStepFunction(backward: value > 0)
+
+while value != 0
+{
+    print("\(value)...")
+    value = moveNearerZero(value)
+}
+
+print("Cero!!!!!")
 
