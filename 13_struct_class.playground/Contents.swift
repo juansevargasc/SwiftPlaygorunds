3d9bd4ce3befe6c4e7c6b9eeb88a734a9b9bdf8a
Juanse <jsvargasc@unal.edu.co>
2020-08-25 20:31:57 -0500

    Classes

--- 13_struct_class.playground/Contents.swift
+++ 13_struct_class.playground/Contents.swift
@@ -45,4 +45,73 @@ print(someVideoMode.frameRate)
 //someResolution.height = 30.0
 
 
-//Structs. Estructuras, datos copiados por valor
+
+//Tue. Aug 25
+//Structs. Estructuras, datos copiados por valor. Tipos Evaluados
+let vga = Resolution(width: 640, height: 480)
+vga.height
+vga.width
+
+let hd = Resolution(width: 1920, height: 1080)
+
+var cinema = hd
+print("\(cinema.width) X \(cinema.height)")
+cinema.width = 2048
+
+print("\(cinema.width) X \(cinema.height)")
+
+print("\(hd.width) X \(hd.height)")
+
+
+enum CompassPoint
+{
+    case north, south, east, west
+}
+
+var currentDirection = CompassPoint.north
+var oldDirection = currentDirection
+
+currentDirection = .south
+
+print(currentDirection)
+print(oldDirection)
+
+/*
+ Los enumerados y las estructuras son tipos evaluados.
+ Se están copiando los valores. Tienen valores que se copian
+ de un lado al otro. Una no inmuta la otra
+ */
+
+
+
+//Class. Las clases son tipos de dato por referencia
+//Una afecta a la otra
+
+
+let tenEighty = VideoMode()
+
+tenEighty.resolution = hd
+tenEighty.interlaced = true
+tenEighty.name = "1080i"
+tenEighty.frameRate = 25
+
+let alsoTenEighty = tenEighty
+
+alsoTenEighty.frameRate = 30.0
+tenEighty
+
+/*
+ Tipo de dato por referencia.
+ Una copia del objeto, cambia al original.
+ En este caso, ambos objetos se refieren a
+ la misma posición de memoria. Por tanto son
+ el mismo.
+ */
+
+if tenEighty === alsoTenEighty //!== Comparación de punteros. Comparación de referencias.
+{
+    print("Son el mismo objeto")
+}else{
+    print("Son diferentes")
+}
+
