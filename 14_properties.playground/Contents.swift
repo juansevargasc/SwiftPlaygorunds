d5da19c23cd371fb1cc0f09b4f0f2a2761b22df6
Juanse <jsvargasc@unal.edu.co>
2020-08-28 12:15:50 -0500

    gitignore changes

--- 14_properties.playground/Contents.swift
+++ 14_properties.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-//Stores Properties. Se pueden crear en el interior de una clase/estructura
+//Stored Properties. Se pueden crear en el interior de una clase/estructura
 struct FixedLengthRange{
     var firstValue: Int
     let length: Int
@@ -10,3 +10,7 @@ var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
 rangeOfThreeItems.firstValue = 6
 let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
 //rangeOfFourItems.firstValue = 5  - Error típico
+
+
+//Lazy Prperties
+
